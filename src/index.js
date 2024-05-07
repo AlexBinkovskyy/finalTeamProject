cd3b616f777be2f2907854dc91d9ae178045f352
AlexBinkovskyy <alex_bin@ukr.net>
2024-05-07 19:40:39 +0300

    Default styles fix

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider>
