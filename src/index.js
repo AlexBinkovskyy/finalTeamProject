bc3f2c1beb417b849489459508621944259d49db
AlexBinkovskyy <alex_bin@ukr.net>
2024-05-07 19:46:10 +0300

    import fix

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import 'modern-normalize';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
