1d7c918a6667ce14934ffe67deb30cf992f8a219
AlexBinkovskyy <alex_bin@ukr.net>
2024-05-07 14:02:05 +0300

    add redux, normalize styles, index.html

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/finalTeamProject'>
-      <App />
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter basename="/finalTeamProject">
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
