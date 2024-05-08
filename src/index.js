4e795a5f6f66e40ed96e8ae77e231fd2ad09ba0a
AlexBinkovskyy <alex_bin@ukr.net>
2024-05-08 13:35:39 +0300

    state control disabling

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import 'modern-normalize';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+// import { BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
