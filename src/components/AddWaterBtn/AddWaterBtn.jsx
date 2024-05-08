6fa505e0e36c39335398b2619aed9a3531cc8ed2
unknown <edovguy97@gmail.com>
2024-05-09 01:49:17 +0300

    Fonts + variables + sprite + img

--- src/components/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/AddWaterBtn/AddWaterBtn.jsx
@@ -1,10 +1,5 @@
 import css from './AddWaterBtn.module.css';
-import { ReactComponent as IconEdit2 } from '../';
+
 export default function AddWaterBtn() {
-  return (
-    <>
-      <IconEdit2 />
-      <button className={css.test}>test</button>
-    </>
-  );
+  return <></>;
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import 'modern-normalize';
-// import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Provider> */}
-    {/* <BrowserRouter basename="/finalTeamProject"> */}
-    <App />
-    {/* </BrowserRouter> */}
-    {/* </Provider> */}
+    <Provider>
+      <BrowserRouter basename="/finalTeamProject">
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
