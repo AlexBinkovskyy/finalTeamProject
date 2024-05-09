f8d7f35616572d56e23727909bdbcc7bb607b756
OnishYurii <onishchenkoyurii@gmail.com>
2024-05-09 19:39:12 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import AddWaterBtn from './AddWaterBtn/AddWaterBtn';
-import SignUpForm from './SignUpForm/SignUpForm';
-import SignInForm from './SignInForm/SignInForm';
 export const App = () => {
   return (
     <>
