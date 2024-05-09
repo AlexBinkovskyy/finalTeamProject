50bd5859df3dfb17223ba9c108337fc1ff780268
Tkachenko_Yuriy <uvt86@ukr.net>
2024-05-09 23:11:31 +0300

    add modal_component

--- src/Modals/Modal/Modal.jsx
+++ src/Modals/Modal/Modal.jsx
@@ -1,3 +1,30 @@
-export default function Modal(){
-    return <></>
-}
+import Modal from 'react-modal';
+
+export const modalWrapper = WrappedComponent => {
+  return function ModalWrapper(props) {
+    Modal.setAppElement('#root');
+
+    return (
+      <div>
+        <WrappedComponent />
+        <Modal
+          isOpen={props.isOpen}
+          onRequestClose={props.isClose}
+          shouldCloseOnOverlayClick={true}
+          shouldCloseOnEsc={true}
+          contentLabel="modalWrapper"
+        >
+          {props.children}
+        </Modal>
+      </div>
+    );
+  };
+};
+
+const WrappedComponent = ({ children }) => {
+  return <>{children}</>;
+};
+
+const ComponentWithModal = modalWrapper(WrappedComponent);
+
+export default ComponentWithModal;
--- src/Modals/UserSettingsModal/UserSettingsModal.jsx
+++ src/Modals/UserSettingsModal/UserSettingsModal.jsx
@@ -1,5 +1,18 @@
+import { IoIosCloseCircleOutline } from 'react-icons/io';
+import ComponentWithModal from '../Modal/Modal';
+import UserSettingsForm from '../UserSettingsForm/UserSettingsForm';
 import css from './UserSettingsModal.module.css';
 
-export default function UserSettingsModal() {
-  return <></>;
+export default function UserSettingsModal({ isOpen, isClose }) {
+  return (
+    <ComponentWithModal isOpen={isOpen} isClose={isClose}>
+      <div>
+        <h3>Settings</h3>
+        <UserSettingsForm />
+        <button onClick={isClose}>
+          <IoIosCloseCircleOutline />
+        </button>
+      </div>
+    </ComponentWithModal>
+  );
 }
