81129d368de855e2a47b7ef83c552b3399ee48da
DmytroLynnyk <138771794+DmytroLynnyk@users.noreply.github.com>
2024-05-09 19:37:51 +0300

    Correct lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
-import { useEffect, lazy } from 'react';
-// import { useEffect } from 'react';
+// import { useEffect, lazy } from 'react';
+import { useEffect } from 'react';
+
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
