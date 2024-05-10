23d2aab1e18020a91f5159f25306079dda1e58c5
Nadya <117812229+Ntimrova@users.noreply.github.com>
2024-05-10 22:30:12 +0300

    css

--- src/Modals/DeleteWaterModal/DeleteWaterModal.jsx
+++ src/Modals/DeleteWaterModal/DeleteWaterModal.jsx
@@ -1,36 +1,73 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+
+
+
+
+// export const DeleteWaterModal = () => {
+//   const [modalIsOpen, setIsOpen] = useState(false);
+
+//   const openModal = () => {
+//     setIsOpen(true);
+//   };
+
+//   const closeModal = () => {
+//     setIsOpen(false);
+//   };
+
+//   // Функция handleDelete
+//   const handleDelete = () => {
+//     // Здесь можно добавить логику для удаления записи
+//     closeModal();
+//   };
+
+//   return (
+//     <>
+//       <button onClick={openModal}>Open Modal</button>
+//       <ComponentWithModal isOpen={modalIsOpen} isClose={closeModal}>
+//         <div className={css.modalOverlay}>
+//           <div className={css.modalContent}>
+//             <button className={css.closeButton} onClick={closeModal}> a
+//               {/* <svg className={css.iconClose}>
+//                 <use href={`${IconX}#icon`}></use>
+//               </svg> */}
+//             </button>
+//             <h2 className={css.modalTitle}>Delete entry</h2>
+//             <p className={css.modalQuestion}>
+//               Are you sure you want to delete the entry?
+//             </p>
+//             <div className={css.buttonContainer}>
+//               <button className={css.deleteButton} onClick={handleDelete}>
+//                 Delete
+//               </button>
+//               <button className={css.cancelButton} onClick={closeModal}>
+//                 Cancel
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//       </ComponentWithModal>
+//     </>
+//   );
+// };
 import ComponentWithModal from '../Modal/Modal';
 import css from './DeleteWaterModal.module.css';
-// import IconX from 'src/image/sprite.svg'
+import IconX from '../../image/sprite.svg';
 
-
-export const DeleteWaterModal = () => {
-  const [modalIsOpen, setIsOpen] = useState(false);
-
-  const openModal = () => {
-    setIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-
+export const DeleteWaterModal = ({ isOpen, isClose }) => {
   // Функция handleDelete
   const handleDelete = () => {
     // Здесь можно добавить логику для удаления записи
-    closeModal();
+    isClose();
   };
 
   return (
     <>
-      <button onClick={openModal}>Open Modal</button>
-      <ComponentWithModal isOpen={modalIsOpen} isClose={closeModal}>
+      <ComponentWithModal isOpen={isOpen} isClose={isClose}>
         <div className={css.modalOverlay}>
-          <div className={css.modalContent}>
-            <button className={css.closeButton} onClick={closeModal}> a
-              {/* <svg className={css.iconClose}>
-                <use href={`${IconX}#icon`}></use>
-              </svg> */}
+            <button className={css.closeButton} onClick={isClose}> 
+              <svg className={css.iconClose}>
+                <use href={`${IconX}#IconX`}></use>
+              </svg>
             </button>
             <h2 className={css.modalTitle}>Delete entry</h2>
             <p className={css.modalQuestion}>
@@ -40,12 +77,11 @@ export const DeleteWaterModal = () => {
               <button className={css.deleteButton} onClick={handleDelete}>
                 Delete
               </button>
-              <button className={css.cancelButton} onClick={closeModal}>
+              <button className={css.cancelButton} onClick={isClose}>
                 Cancel
               </button>
             </div>
           </div>
-        </div>
       </ComponentWithModal>
     </>
   );
