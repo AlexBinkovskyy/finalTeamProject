38885cc949ecb7c7dbb5776ea1e785b6e93418bf
AlexBinkovskyy <alex_bin@ukr.net>
2024-05-10 19:25:45 +0300

    fix css

--- src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
+++ src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
@@ -1,5 +1,5 @@
 import UserPanel from 'components/UserPanel/UserPanel';
-import css from './WaterDetailedInfo.module.css';
+// import css from './WaterDetailedInfo.module.css';
 import DailyInfo from 'components/DailyInfo/DailyInfo';
 import MonthInfo from 'components/MonthInfo/MonthInfo';
 
