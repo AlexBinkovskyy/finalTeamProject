a73316a109b3e148c9a5090e89372f18e0e68eae
Nadya <117812229+Ntimrova@users.noreply.github.com>
2024-05-10 13:11:25 +0300

    WaterModal

--- src/Modals/WaterModal/WaterModal.jsx
+++ src/Modals/WaterModal/WaterModal.jsx
@@ -1,5 +1,31 @@
+import React, { useState } from 'react';
+import WaterForm from './WaterForm';
+import ComponentWithModal from '../Modal/Modal';
 import css from './WaterModal.module.css';
 
-export default function WaterModal() {
-  return <></>;
-}
+export const WaterModal = ({ operationType, onSubmit, defaultValues }) => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <ComponentWithModal isOpen={modalIsOpen} isClose={closeModal}>
+      <button onClick={openModal}>Open Modal</button>
+      <div className={css.modal}>
+        <div className={css.modalContent}>
+          <span className={css.close} onClick={closeModal}>&times;</span>
+          <h2>{operationType === 'add' ? 'Add Water' : 'Edit the entered amount of water'}</h2>
+          <WaterForm onSubmit={onSubmit} defaultValues={defaultValues} />
+        </div>
+      </div>
+    </ComponentWithModal>
+  );
+};
+
+export default WaterModal;
