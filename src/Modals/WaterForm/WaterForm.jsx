8a5beebb202c90ecc5b15e01e14d91368500c906
Nadya <117812229+Ntimrova@users.noreply.github.com>
2024-05-12 21:44:11 +0300

    upd

--- src/Modals/WaterForm/WaterForm.jsx
+++ src/Modals/WaterForm/WaterForm.jsx
@@ -1,37 +1,19 @@
-// import css from './WaterForm.module.css';
-
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import * as Yup from 'yup';
+import css from './WaterForm.module.css'; 
 
 const schema = Yup.object().shape({
   waterAmount: Yup.number()
     .required('Water amount is required')
     .positive('Water amount must be positive'),
-    time: Yup.string() 
+  time: Yup.string() 
     .required('Time is required')
     .matches(/^([01]\d|2[0-3]):([0-5]\d)$/, 'Invalid time format'),
 });
 
 const WaterForm = () => {
-  // const { register, handleSubmit, errors } = useForm({
-  //   validationSchema: schema,
-  // });
-  // console.log(errors);
-
-  // const onSubmit = data => {
-  //   console.log(data); 
-  // };
-
-  const [waterAmount, setWaterAmount] = useState(50); // Значення води за замовчуванням - 50ml
-  // const [currentTime, setCurrentTime] = useState(getCurrentTime());
-
-  // function getCurrentTime() {
-  //   const now = new Date();
-  //   const hour = now.getHours().toString().padStart(2, '0');
-  //   const minute = now.getMinutes().toString().padStart(2, '0');
-  //   return `${hour}:${minute}`;
-  // }
+  const [waterAmount, setWaterAmount] = useState(50);
 
   const handleIncrement = () => {
     setWaterAmount(prevAmount => prevAmount + 50);
@@ -49,27 +31,25 @@ const WaterForm = () => {
   };
 
   return (
-    <form >
-      <div>
+    <form className={css.waterForm}> 
+      <div className={css.inputGroup}>
         <label htmlFor="waterAmount">Amount of water:</label>
         <div>
-          <button type="button" onClick={handleDecrement}>-</button>
-          <span onChange={handleInputChange}>{waterAmount} ml </span>
-          <button type="button" onClick={handleIncrement}>+</button>
+          <button type="button" onClick={handleDecrement}  className={css.buttonIncrement}>-</button>
+          <span  className={css.waterAmount}>{waterAmount} ml </span>
+          <button type="button" onClick={handleIncrement} className={css.buttonIncrement}>+</button>
         </div>
       </div>
-      <div>
+      <div className={css.inputGroup}>
         <label htmlFor="time">Recording time:</label>
-        <input type="text" name="time"  />
-        
+        <input type="text" name="time" />
       </div>
-      <div>
+      <div className={css.inputGroup}>
         <label htmlFor="water">Enter the value of water used:</label>
         <input type="number" name="waterAmount" onChange={handleInputChange} />
-        
       </div>
       <div>
-        <button type="submit">Save</button>
+        <button type="submit" className={css.saveBtn}>Save</button>
       </div>
     </form>
   );
