a40318f131e38a81439f8b402e32f736e930111a
PolliBu <8polli.bu@gmail.com>
2024-05-13 23:03:57 +0300

    dailyNorma

--- src/components/WaterDailyNorma/WaterDailyNorma.jsx
+++ src/components/WaterDailyNorma/WaterDailyNorma.jsx
@@ -1,10 +1,15 @@
 import css from './WaterDailyNorma.module.css';
+import { selectDailyNorma } from '../../redux/auth/selectors';
+import { useSelector } from 'react-redux';
 
 export default function WaterDailyNorma() {
+  const dailyNorma = useSelector(selectDailyNorma);
+  const dailyNormaInLiters = (dailyNorma / 1000).toFixed(1);
+
   return (
     <>
       <div className={css.WaterDailyNorma}>
-        <p className={css.norma}>1.5 L</p>
+        <p className={css.norma}>{dailyNormaInLiters} L</p>
         <p className={css.text}>My daily norma</p>
       </div>
     </>
