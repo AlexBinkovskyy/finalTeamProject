0d16d09c086af3333398536c005a1565824798d4
Nadya <117812229+Ntimrova@users.noreply.github.com>
2024-05-14 10:59:16 +0300

    icon close added

--- src/Modals/WaterModal/WaterModal.jsx
+++ src/Modals/WaterModal/WaterModal.jsx
@@ -1,5 +1,6 @@
 import WaterForm  from '../WaterForm/WaterForm';
 import ComponentWithModal from '../Modal/Modal';
+import IconX from '../../image/sprite.svg';
 import css from './WaterModal.module.css';
 
 export const WaterModal = ({ operationType, onSubmit, defaultValues, isOpen, isClose }) => {
@@ -8,7 +9,11 @@ export const WaterModal = ({ operationType, onSubmit, defaultValues, isOpen, isC
     <ComponentWithModal isOpen={isOpen} isClose={isClose}>
       <div className={css.modal}>
         <div className={css.modalContent}>
-          <span className={css.close} >&times;</span>
+        <button className={css.closeButton} onClick={isClose}>
+              <svg className={css.iconClose}>
+                <use href={`${IconX}#IconX`}></use>
+              </svg>
+            </button>
           {operationType === 'add' ? (
           <div>
             <h2>Add Water</h2>
