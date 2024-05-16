2b4a3869b61c756b0a38270e00084724b3b1d4fb
PolliBu <8polli.bu@gmail.com>
2024-05-16 12:57:42 +0300

    const dailyNorma = useSelector(selectGoal)

--- src/components/WaterDailyNorma/WaterDailyNorma.jsx
+++ src/components/WaterDailyNorma/WaterDailyNorma.jsx
@@ -1,15 +1,14 @@
 import css from './WaterDailyNorma.module.css';
-import { selectDailyNorma } from '../../redux/auth/selectors';
+import { selectGoal } from '../../redux/auth/selectors';
 import { useSelector } from 'react-redux';
 
 export default function WaterDailyNorma() {
-  const dailyNorma = useSelector(selectDailyNorma);
-  const dailyNormaInLiters = (dailyNorma / 1000).toFixed(1);
+  const dailyNorma = useSelector(selectGoal);
 
   return (
     <>
       <div className={css.WaterDailyNorma}>
-        <p className={css.norma}>{dailyNormaInLiters} L</p>
+        <p className={css.norma}>{dailyNorma} L</p>
         <p className={css.text}>My daily norma</p>
       </div>
     </>
