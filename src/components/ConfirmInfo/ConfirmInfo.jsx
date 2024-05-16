33a673b820c7bffacd4326403e259d8616fbad69
OnishYurii <onishchenkoyurii@gmail.com>
2024-05-16 18:41:20 +0300

    deleted non-used data

--- src/components/ConfirmInfo/ConfirmInfo.jsx
+++ src/components/ConfirmInfo/ConfirmInfo.jsx
@@ -1,17 +1,7 @@
 import { Link } from 'react-router-dom';
 import css from './ConfirmInfo.module.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { resendMail } from '../../redux/auth/operations';
-import { selectUser } from '../../redux/auth/selectors';
 
 export default function ConfirmInfo() {
-  const user = useSelector(selectUser);
-  const dispatch = useDispatch();
-
-  const handleClick = user => {
-    dispatch(resendMail({ email: user.email }));
-  };
-
   return (
     <div>
       <h1 className={css.title}>Confirm Your Email Address</h1>
