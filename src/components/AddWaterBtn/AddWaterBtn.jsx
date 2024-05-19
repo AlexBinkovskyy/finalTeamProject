40356f5b7497172d86cec3fac528e7b4fb14ca68
Nadya <117812229+Ntimrova@users.noreply.github.com>
2024-05-19 14:42:24 +0300

    Fixed AddWatterButton so that user can not add water in the future

--- src/components/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/AddWaterBtn/AddWaterBtn.jsx
@@ -1,16 +1,32 @@
 import { useState } from 'react';
 import { GoPlus } from 'react-icons/go';
 import { WaterModal } from '../../Modals/WaterModal/WaterModal';
+import { selectChosenDate } from '../../redux/water/selectors';
 import css from './AddWaterBtn.module.css';
+import { useSelector } from 'react-redux';
 
 export default function AddWaterBtn({ selectedDay = new Date() }) {
   const [waterModalIsOpen, setwaterModalIsOpen] = useState(false);
   const [operationType, setOperationType] = useState('');
 
+  const chosenDate = useSelector(selectChosenDate);
+
+  function getCurrentDateFormatted() {
+    const today = new Date();
+    const day = String(today.getDate()).padStart(2, '0');
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const year = today.getFullYear();
+    return `${day}.${month}.${year}`;
+  }
+
+  const currentDateFormatted = getCurrentDateFormatted();
+
   const openWaterModal = () => {
-    setwaterModalIsOpen(true);
-    setOperationType('add');
-    document.body.style.overflow = 'hidden';
+    if (chosenDate <= currentDateFormatted) {
+      setwaterModalIsOpen(true);
+      setOperationType('add');
+      document.body.style.overflow = 'hidden';
+    }
   };
 
   const closeWaterModal = () => {
@@ -31,6 +47,7 @@ export default function AddWaterBtn({ selectedDay = new Date() }) {
         </div>
         <div>Add water</div>
       </button>
+
       {waterModalIsOpen && (
         <WaterModal
           onSubmit={onSubmit}
