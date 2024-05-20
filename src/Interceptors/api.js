ac13d9f0d6d4d150c9e9528131b454edadbea5ed
AlexBinkovskyy <alex_bin@ukr.net>
2024-05-21 00:24:35 +0300

    api

--- src/Interceptors/api.js
+++ src/Interceptors/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://finalteamproject-backend.onrender.com/api',
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 export default api;
