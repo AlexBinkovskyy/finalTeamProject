95292ce1dd80d96e94d661acff80e3d99d08f283
Nadya <117812229+Ntimrova@users.noreply.github.com>
2024-05-21 12:06:29 +0300

    i18next

---
+++ src/components/LanguageSwitcher/LanguageSwitcher.jsx
@@ -0,0 +1,18 @@
+import { useTranslation } from 'react-i18next';
+
+const LanguageSwitcher = () => {
+  const { i18n } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+
+  return (
+    <div>
+      <button onClick={() => changeLanguage('en')}>English</button>
+      <button onClick={() => changeLanguage('uk')}>Українська</button>
+    </div>
+  );
+};
+
+export default LanguageSwitcher;
