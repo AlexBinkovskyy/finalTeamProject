1e5809474565bd03be0f26f5dbd17c22dd3e3f59
DmytroLynnyk <138771794+DmytroLynnyk@users.noreply.github.com>
2024-05-23 19:39:15 +0300

    api correct

--- src/Interceptors/api.js
+++ src/Interceptors/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //  baseURL: 'https://finalteamproject-backend.onrender.com/api',
-  baseURL: 'http://localhost:10000/api',
+  baseURL: 'https://finalteamproject-backend.onrender.com/api',
+  // baseURL: 'http://localhost:10000/api',
   // withCredentials: true,
 });
 
