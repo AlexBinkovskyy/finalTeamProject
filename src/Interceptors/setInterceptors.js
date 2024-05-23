48b448ccc0c63d9dd21e54ed191c00be24c87d8b
AlexBinkovskyy <alex_bin@ukr.net>
2024-05-23 13:21:42 +0300

    refresg atleast done

--- src/Interceptors/setInterceptors.js
+++ src/Interceptors/setInterceptors.js
@@ -14,7 +14,10 @@ const setInterceptors = () => {
       if (error.response) {
         if (error.response.status === 401 || error.response.status === 500) {
           const originalRequest = error.config;
-          originalRequest._mark = 'restored';
+          if (originalRequest._retry) {
+            return Promise.reject(error);
+          }
+          originalRequest._retry = true;
 
           console.log(originalRequest);
 
@@ -22,87 +25,35 @@ const setInterceptors = () => {
           const id = jwtDecode(token);
           const refreshToken = localStorage.getItem(`userId_${id.id}`);
 
-          await store
-              .dispatch(refreshUserTokens({ refreshToken }))
-              .then(item => {
-                console.log('item', item);
+          try {
+            const { payload } = await store.dispatch(
+              refreshUserTokens({ refreshToken })
+            );
+            console.log('item', payload);
 
-                const newAccessToken = item.payload.accessToken;
-                console.log(newAccessToken);
-                const newRefreshToken = item.payload.refreshToken;
-                localStorage.setItem(`userId_${id.id}`, newRefreshToken);
+            const newAccessToken = payload.accessToken;
+            const newRefreshToken = payload.refreshToken;
+            localStorage.setItem(`userId_${id.id}`, newRefreshToken);
 
-                setAuthHeader(newAccessToken);
+            setAuthHeader(newAccessToken);
 
-                console.log('req', originalRequest.headers.Authorization);
-                originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-                console.log('req2', originalRequest.headers.Authorization);
+            console.log('req', originalRequest.headers.Authorization);
+            originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+            console.log('req2', originalRequest.headers.Authorization);
 
-                return api(originalRequest);
-              });
-      
+            return api(originalRequest);
+          } catch (refreshError) {
+            return Promise.reject(refreshError);
+          }
         }
         if (error.response.status === 403) {
           store.dispatch(tokenIsInvalid());
           history.push('/finalTeamProject/signin');
         }
       }
-      // return Promise.reject(error);
+      return Promise.reject(error);
     }
   );
 };
 
 export default setInterceptors;
-
-
-
-// const history = createBrowserHistory();
-
-// const setInterceptors = () => {
-//   api.interceptors.response.use(
-//     response => response,
-//     async error => {
-//       if (error.response) {
-//         if (error.response.status === 401 || error.response.status === 500) {
-//           const originalRequest = error.config;
-//           if (originalRequest._retry) {
-//             return Promise.reject(error); // Уникаємо циклічного виклику
-//           }
-//           originalRequest._retry = true;
-
-//           console.log(originalRequest);
-
-//           const token = originalRequest.headers.Authorization.split(' ')[1];
-//           const id = jwtDecode(token);
-//           const refreshToken = localStorage.getItem(`userId_${id.id}`);
-
-//           try {
-//             const { payload } = await store.dispatch(refreshUserTokens({ refreshToken }));
-//             console.log('item', payload);
-
-//             const newAccessToken = payload.accessToken;
-//             const newRefreshToken = payload.refreshToken;
-//             localStorage.setItem(`userId_${id.id}`, newRefreshToken);
-
-//             setAuthHeader(newAccessToken);
-
-//             console.log('req', originalRequest.headers.Authorization);
-//             originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-//             console.log('req2', originalRequest.headers.Authorization);
-
-//             return api(originalRequest); // Повертаємо обіцянку повторного запиту
-//           } catch (refreshError) {
-//             return Promise.reject(refreshError);
-//           }
-//         }
-//         if (error.response.status === 403) {
-//           store.dispatch(tokenIsInvalid());
-//           history.push('/finalTeamProject/signin');
-//         }
-//       }
-//       return Promise.reject(error);
-//     }
-//   );
-// };
-
-// export default setInterceptors;
