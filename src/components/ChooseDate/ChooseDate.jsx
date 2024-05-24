94e8c4a0d0f90b71e03ec83c2abf2d6a4aff3d91
OnishYurii <onishchenkoyurii@gmail.com>
2024-05-24 18:04:11 +0300

    translate in progress

--- src/components/ChooseDate/ChooseDate.jsx
+++ src/components/ChooseDate/ChooseDate.jsx
@@ -1,15 +1,17 @@
 import css from './ChooseDate.module.css';
 import { format } from 'date-fns';
+import { useTranslation } from 'react-i18next';
 
 export default function ChooseDate({ selectedDate }) {
   const todayDate = format(new Date(), 'd MMMM');
   const incommingDate = format(selectedDate, 'd MMMM');
+  const { t } = useTranslation();
 
   return (
     <>
       <div>
         {incommingDate === todayDate ? (
-          <h3 className={css.date}>Today</h3>
+          <h3 className={css.date}>{t('chooseDate.today')}</h3>
         ) : (
           <h3 className={css.date}>{incommingDate}</h3>
         )}
